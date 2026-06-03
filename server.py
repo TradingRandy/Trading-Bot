ec0299a6f2168c919ab0dd81cd6def4675d8b9ca
TradingRandy <randymeier14@gmail.com>
2026-06-03 21:54:56 +0200

    server.py

--- server.py
+++ server.py
@@ -1,13 +1,25 @@
+import requests
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+def send_telegram(message):
+    token = os.environ.get("8701563388:AAFuXphtL24yK-BrxS1SU8MB6tsk63JKMoY")
+    chat_id = os.environ.get("5562976664")
+
+    if not token or not chat_id:
+        print("Missing env vars")
+        return
+
+    url = f"https://api.telegram.org/bot8701563388:AAFuXphtL24yK-BrxS1SU8MB6tsk63JKMoY/sendMessage"
+
+    requests.post(url, data={
+        "chat_id": chat_id,
+        "text": message
+    })
+
 @app.route("/")
 def home():
+    send_telegram("🧠 Trading Brain ONLINE TEST")
     return "Trading Bot läuft 🔥"
-
-# WICHTIG für Render:
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
